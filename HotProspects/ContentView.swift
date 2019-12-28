cf82364ee6c1077dd7ec5a9b30fd5ba6c893384b
Mark Booth <mark@markandpol.com>
2019-12-28 09:59:55 +0000

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Mark Booth on 28/12/2019.
+//  Copyright © 2019 Mark Booth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
