c1ad42870a65a3ac491c8234f945d3bb915d1ba1
Mark Booth <mark@markandpol.com>
2019-12-30 10:24:41 +0000

    Add notification for reminders and Icon in Everyone listing to show contacted or not.

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import CodeScanner
+import UserNotifications
 
 struct ProspectsView: View {
     @EnvironmentObject var prospects: Prospects
@@ -47,12 +48,24 @@ struct ProspectsView: View {
                     VStack(alignment: .leading) {
                         Text(prospect.name)
                             .font(.headline)
-                        Text(prospect.emailAddress )
-                            .foregroundColor(.secondary)
+                        HStack {
+                            Text(prospect.emailAddress )
+                                .foregroundColor(.secondary)
+                            if prospect.isContacted {
+                                Image(systemName: "checkmark.circle")
+                            } else {
+                                Image(systemName: "questionmark.diamond")
+                            }
+                        }
                     }
                     .contextMenu{
                         Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted"){
                             self.prospects.toggle(prospect)
+                        }
+                        if !prospect.isContacted {
+                            Button("Remind Me") {
+                                self.addNotification(for: prospect)
+                            }
                         }
                     }
                     
@@ -84,6 +97,35 @@ struct ProspectsView: View {
             
         case .failure(let error):
             print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+// testing            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]){
+                    success, error in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("Doh")
+                    }
+                }
+            }
         }
     }
 }
